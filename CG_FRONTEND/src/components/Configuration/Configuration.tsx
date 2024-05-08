18026f091f1cc2c9c779a8f5b2d333868f4e1e93
Lucas-Jacobsen <97192501+Lucas-Jacobsen@users.noreply.github.com>
2024-05-08 13:52:50 -0500

    Update Configuration.tsx flow

--- CG_FRONTEND/src/components/Configuration/Configuration.tsx
+++ CG_FRONTEND/src/components/Configuration/Configuration.tsx
@@ -1,7 +1,8 @@
 function Configuration() {
  
-  
-
+  /*Make table -> fetch full db-> identify system -> 1 input at a time -> identify next part input -> find corresponding parts per description 
+  -> find corresponding parts per used -> populate two menus to separate the two recomendations -> once next part is inputed remove tables and move to the next "next" part 
+*/
   return (
     <div>
     
